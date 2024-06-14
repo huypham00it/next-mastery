1b58ec03bca5995cb47c47c124d67b144ff04d0c
huy <huypham00.it@gmail.com>
2024-06-14 15:50:12 +0700

    feat:cloudinary with image blur placeholder

---
+++ app/page.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+import getBase64ImageUrl from "@/utils/generateBlurPlaceholder";
+import { ImageProps } from "@/utils/types";
+import cloudinary from "@/utils/cloudinary";
+
+export default async function Home() {
+  const results = await cloudinary.v2.search
+    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+    .sort_by("public_id", "desc")
+    .max_results(400)
+    .execute();
+  let reducedResults: ImageProps[] = [];
+
+  let i = 0;
+  for (let result of results.resources) {
+    reducedResults.push({
+      id: i,
+      height: result.height,
+      width: result.width,
+      public_id: result.public_id,
+      format: result.format,
+    });
+    i++;
+  }
+
+  const blurImagePromises = results.resources.map((image: ImageProps) => {
+    return getBase64ImageUrl(image);
+  });
+  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+
+  for (let i = 0; i < reducedResults.length; i++) {
+    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+        {reducedResults.map(({ id, public_id, format, blurDataUrl }) => (
+          <Link
+            key={id}
+            href={`/?photoId=${id}`}
+            as={`/p/${id}`}
+            shallow
+            className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+          >
+            <Image
+              alt="Next.js Conf photo"
+              className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+              style={{ transform: "translate3d(0, 0, 0)" }}
+              placeholder="blur"
+              blurDataURL={blurDataUrl}
+              src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+              width={720}
+              height={480}
+              sizes="(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw"
+            />
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
---
+++ utils/generateBlurPlaceholder.ts
@@ -0,0 +1,23 @@
+import sharp from "sharp";
+import { ImageProps } from "./types";
+
+const cache = new Map<ImageProps, string>();
+
+export default async function getBase64ImageUrl(
+  image: ImageProps
+): Promise<string> {
+  let url = cache.get(image);
+  if (url) {
+    return url;
+  }
+  const response = await fetch(
+    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.public_id}.${image.format}`
+  );
+
+  const buffer = await response.arrayBuffer();
+  const blurredImage = await sharp(buffer).blur(7).toBuffer();
+
+  url = `data:image/jpeg;base64,${blurredImage.toString("base64")}`;
+  cache.set(image, url);
+  return url;
+}
